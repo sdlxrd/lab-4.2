bd54b7b78f00176b47d6366bde23a396951e59c6
sdlxrd <verysadforever26@gmail.com>
2021-10-06 19:02:23 +0300

    Add project files.

---
+++ lab 4.2/lab 4.2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = abs(4 * x - 1);
+		if (x < 0)
+			B = pow(x, 7) - 2 * x;
+		else
+			if (x >= 3)
+				B = pow(x, 4) + exp(pow(x, 2) + 3);
+			else
+				B = atan((exp(x) + 1) / 8);
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
